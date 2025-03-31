09682b2e0a3fb81fac880624ba06a7c849796077
sihaun <goddb0522@naver.com>
2025-03-31 16:36:17 +0900

    add comment

--- server/server.py
+++ server/server.py
@@ -6,7 +6,6 @@ import time
 import threading
 
 USER_DB = "users.json"
-SESSION_DB = {}  # 쿠키 저장용
 
 class Server(socket.socket):
     def __init__(self, port : int):
@@ -103,38 +102,59 @@ class Server(socket.socket):
         return ("\r\n".join(response), body)
 
     def register_handler(self, id : str, password : str) -> tuple:
+        '''
+        client가 /register로 접근했을 때 처리하는 함수.
+
+        user.json에서 user database를 받아옴 -> 입력받은 id가 존재하는 지 확인.
+        존재하지 않으면 id, pw 쌍으로 계정 생성하고 계정 정보를 user database에 저장.
+        
+        id : client id. 등록하고자 하는 id
+        password : client password. 등록하고자 하는 pw
+        
+        return : tuple(response, None). register_handler는  byte data를 생성하지 않으므로 None.'''
         users = self.load_users()
-        if id in users:
+        if id in users: # 이미 user가 있을 때
             return self._create_response_str("400 Bad Request", body="REGISTER_FAILED: User already exists")
         
         users[id] = {"pw" : password, "key" : {"value" : self.default_key, "expiry_time" : 0} }
         self.save_users(users)
-        return self._create_response_str("200 OK", body="REGISTER_SUCCESS")
+        return self._create_response_str("200 OK", body="REGISTER_SUCCESS") # 등록 완료
 
     def login_handler(self, id : str, password : str) -> tuple:
+        '''
+        client가 /login로 접근했을 때 처리하는 함수.
+
+        users.json에서 user database를 받아옴 -> 입력받은 id와 pw 쌍이 존재하는 지 확인
+        
+        id : login id. 로그인하고자 하는 id
+        password : login password. 로그인하고자 하는 pw
+        
+        return : tuple(response, None). login_handler는  byte data를 생성하지 않으므로 None.'''
         users = self.load_users()
         try:
-            if users.get(id)["pw"] == password:
-                session_id = f"{id}"
-                SESSION_DB[session_id] = id  # 세션 저장
-
-                headers = [f"Set-Cookie: session_id={session_id}; Max-Age=3600"]
-                return self._create_response_str("200 OK", headers, body="LOGIN_SUCCESS")
-        except:
+            if users.get(id)["pw"] == password: # login success
+                return self._create_response_str("200 OK", body="LOGIN_SUCCESS")
+        except: # get(id) == None or get(id) != password
             pass
         return self._create_response_str("401 Unauthorized", body="LOGIN_FAILED")
     
-    def privilege_handler(self, id : dict, key_is_valid : bool=True, check : bool=True) -> tuple:
+    def privilege_handler(self, id : dict, key_is_valid : bool=True) -> tuple:
+        '''
+        client가 /privilege로 접근했을 때 처리하는 함수.
+
+        키가 유효하다면 이미 권한이 상승된 상태이므로 409 도출.
+        키가 유효하지 않다면 권한이 상승되지 않았거나 키의 유효기간이 만료된 상태이므로 키를 새로 발급해줌.
+        
+        id : login id. 권한을 상승하고자 하는 id
+        key_is_vaild : client key의 유효 여부
+        
+        return : tuple(response, None). login_handler는  byte data를 생성하지 않으므로 None.'''
         users = self.load_users()  
 
-        if check:
-            if key_is_valid:
-                return self._create_response_str("200 OK")
-            return self._create_response_str("401 Unauthorized")
-        
-        if key_is_valid:
+        if key_is_valid: # 이전에 키가 발급되었음
             return self._create_response_str("409 Conflict", body="PRIVILEGE_ALREADY_CHANGED")
         
+        # 키 발급
         users.get(id)["key"]["value"] = "ABCD"
         users.get(id)["key"]["expiry_time"] = time.time() + 3600
         self.save_users(users)
@@ -142,7 +162,18 @@ class Server(socket.socket):
         headers = [f"Set-Cookie: key=ABCD; Max-Age=3600"]
         return self._create_response_str("200 OK", headers, body="PRIVILEGE_CHANGED")
     
-    def image_downloader(self, url : str) -> str:
+    def image_downloader(self, url : str) -> tuple:
+        '''
+        client가 /images로 접근했을 때 처리하는 함수
+
+        client가 요청한 이미지 url이 존재하면 이미지를 byte data로 전달 
+        존재하지 않으면 404 도출
+
+        url : client가 요청한 image file
+
+        return : image가 존재하면 response와 byte data를 튜플로 전달.
+                 image가 존재하지 않으면 response와 None을 전달.
+        '''
         if os.path.exists(url):
             with open(url, "rb") as f:
                 image_data = f.read()
@@ -150,6 +181,17 @@ class Server(socket.socket):
         return self._create_response_str("404 Not Found", body="Image not found")
 
     def request_handler(self, request : str) -> tuple:
+        '''
+        client로부터 받은 request정보를 처리하는 함수
+        
+        request를 method, path, headers, body로 나눔.
+        method와 path를 보고 처리해야할 함수(handler)에 body data를 보냄.
+        
+        request : client로부터 받은 request
+        
+        return : handler의 return 값. client에 다시 보낼 response.
+                response에 byte data가 없을 시 (response, None)
+                response에 byte data가 존재하면 (response, byte data) 형식'''
         print(request)
         lines = request.split("\r\n")
         method, path, _ = lines[0].split(" ")
@@ -168,8 +210,7 @@ class Server(socket.socket):
             data = json.loads(body)
             id = data["username"]
             valid = self._is_valid_key(id)
-
-            return self.privilege_handler(id, valid, check=False)
+            return self.privilege_handler(id, valid)
         
         elif path == "/images": # image
             if method == "HEAD":
@@ -181,15 +222,23 @@ class Server(socket.socket):
             url_data = json.loads(body)
             url = url_data["url"]
             return self.image_downloader(url)
-
-        elif method == "HEAD" and path == "/file": # file
-            if method == "HEAD": # 파일 존재 요청
-                pass
-                    # 파일 다운로드 요청
         
         return self._create_response_str("404 Not Found", body="Page not found")
     
     def _is_valid_key(self, id : str) -> bool:
+        '''
+        client id의 key가 유효한 지 검사하는 함수
+        
+        users.json에서 id를 보고 user key 정보를 받아옴
+
+        False :
+        key["value"] == self.default_key => 권한 상승하지 않음
+        time.time() > key["expiry_time"] => key의 유효 기간이 지남
+
+        id : user id
+
+        return : bool
+        '''
         users = self.load_users()
         key = users.get(id)["key"]
 
@@ -199,6 +248,12 @@ class Server(socket.socket):
         return True
 
 def main(port):
+    '''
+    Start Server
+    
+    서버를 열고 client를 기다림.
+    클라이언트가 접근 => client_handler 실행
+    을 무한 반복'''
     with Server(port) as server:
         while True:
             client_socket, addr = server.accept()
@@ -206,6 +261,9 @@ def main(port):
 
 
 if __name__ == "__main__":
+    '''
+    argv[1] : port
+    threading을 통해 client마다 socket을 새로 생성'''
     if len(sys.argv) != 2:
         print(f"Usage: {sys.argv[0]} <port>")
         sys.exit(1)
